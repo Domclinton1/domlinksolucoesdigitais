ebc1dc674f09fb7a92fb0738c22c0cd68b249a87
Clinton <clinton.admpro29@gmail.com>
2024-08-11 18:08:05 -0300

    Alterações gerais, remoção de media queries

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function() {
     var comentarios = document.querySelectorAll('.comentarios li');
     var index = 0;
@@ -45,51 +44,11 @@ function order(){
     var click = window.document.querySelectorAll(".cta")
     window.open("https://wa.me/5531994038782?text=Ola,%20quero%20fazer%20um%20orçamento%20sobre um projeto...")
 }
-=======
-document.addEventListener("DOMContentLoaded", function() {
-    var comentarios = document.querySelectorAll('.comentarios li');
-    var index = 0;
-
-    function nextComentario() {
-        comentarios[index].classList.remove('active');
-        index = (index + 1) % comentarios.length;
-        comentarios[index].classList.add('active');
-    }
-
-    setInterval(nextComentario, 5000); // Altere o intervalo de troca de slide conforme necessário (em milissegundos)
-});
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const openModalButtons = document.querySelectorAll(".openModal");
-    const modals = document.querySelectorAll(".modal");
-    const closeButtons = document.querySelectorAll(".close");
-
-    openModalButtons.forEach(function(button) {
-        button.addEventListener("click", function() {
-            const modalId = button.getAttribute("data-modal-id");
-            const modal = document.getElementById(modalId);
-            modal.style.display = "block";
-        });
-    });
-
-    closeButtons.forEach(function(button) {
-        button.addEventListener("click", function() {
-            const modal = button.closest(".modal");
-            modal.style.display = "none";
-        });
-    });
-
-    window.addEventListener("click", function(event) {
-        modals.forEach(function(modal) {
-            if (event.target === modal) {
-                modal.style.display = "none";
-            }
-        });
-    });
-});
-function order(){
+function orderAds(){
     var click = window.document.querySelectorAll(".cta")
-    window.open("https://wa.me/5531994038782?text=Ola,%20quero%20fazer%20um%20orçamento%20sobre um projeto...")
+    window.open("https://wa.me/5531994038782?text=Ola,%20quero%20contratar%20seu%20serviço%20de%20tráfego%20pago...")
 }
->>>>>>> b0f03c2f432424f7df99b354e35e27e53f2b6e3e
+function orderDelivery(){
+    var click = window.document.querySelectorAll(".cta")
+    window.open("https://wa.me/5531994038782?text=Ola,%20quero%20adquirir%20meu%20cardápio%20de%20online%20...")
+}
